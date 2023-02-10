3fb85fd5b573c04a9e9924f1c69f0a99dc685453
Felipe Sales <soufeliposales@gmail.com>
2023-02-10 13:24:28 -0300

    chore: removed comments and others details

--- test/main_test.dart
+++ test/main_test.dart
@@ -9,10 +9,6 @@ void main() {
       expect(find.byType(SVGApp), findsOneWidget);
     });
 
-    // ! Here
-    // In `flutter_svg` < 2.0.0, this assertion working.
-    // In `flutter_svg` > 2.0.0, this assertion not working.
-
     testWidgets('Should compare asset', (WidgetTester tester) async {
       await tester.pumpWidget(const SVGApp());
       final findAsset = find.byWidgetPredicate(
